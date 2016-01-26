4ef2a767c967c29104b359bc464f29cd1adb4461
Peter Vargovcik <peter.vargovcik@ark-energy.eu>
2016-01-26 09:39:17 +0000

    Test Changes

--- Test/test.js
+++ Test/test.js
@@ -1,3 +1,26 @@
 /**
  * Created by PeterVargovcik on 25/01/2016.
  */
+casper.start('C:\\Users\\PeterVargovcik\\Documents\\BuildTestTeamCity\\web-ui-test\\index.html', function () {
+    this.test.assertExists('input[name=name]');
+    this.capture('FirstPage.png');
+})
+casper.then(function () {
+    this.fill('form', {
+        'name': 'scream'
+    }, true);
+    this.capture('FirstPageWithTextInForm.png');
+})
+/*
+casper.then(function () {
+    this.click('#trigger');
+    this.capture('trigger.png');
+})
+*/
+casper.then(function () {
+    this.test.assertSelectorHasText('h1', 'The H1');
+    this.capture('EchoPage.png');
+})
+casper.run(function () {
+    this.test.done();
+})
